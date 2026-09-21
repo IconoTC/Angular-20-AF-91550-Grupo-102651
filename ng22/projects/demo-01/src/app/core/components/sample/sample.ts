35678da15f2528fd39441e1bfb633cc58171820d
Alejandro Cerezo <alce65@hotmail.es>
2026-09-21 13:32:29 +0200

    Update sample component

--- ng22/projects/demo-01/src/app/core/components/sample/sample.ts
+++ ng22/projects/demo-01/src/app/core/components/sample/sample.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   imports: [],
   selector: 'ind-sample',
-  styles: ``,
+  styleUrls: ['./sample.css'],
+  styles: `
+    :host {
+      display: block;
+      border: 1px solid #ccc;
+      padding: 1rem;
+    }
+  `,
   template: ` <h2>sample component</h2> `,
+  templateUrl: './sample.html',
 })
 export class Sample {}
