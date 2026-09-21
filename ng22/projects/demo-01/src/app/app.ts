65def80ba5e4c2af5da80e3c6040869d07edbedf
Alejandro Cerezo <alce65@hotmail.es>
2026-09-21 12:43:56 +0200

    Add sample component and use it

--- ng22/projects/demo-01/src/app/app.ts
+++ ng22/projects/demo-01/src/app/app.ts
@@ -1,16 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sample } from './components/sample/sample';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sample],
   selector: 'ind-root',
   styles: [],
   template: `
-    <h1>Hello, {{ title() }}</h1>
-
+    <h1>{{ title() }}</h1>
+    <ind-sample />
     <router-outlet />
   `,
 })
 export class App {
-  protected readonly title = signal('demo-01');
+  protected readonly title = signal('Demo-01');
 }
---
+++ ng22/projects/demo-01/src/app/components/sample/sample.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'ind-sample',
+  styles: ``,
+  template: ` <h2>sample component</h2> `,
+})
+export class Sample {}
