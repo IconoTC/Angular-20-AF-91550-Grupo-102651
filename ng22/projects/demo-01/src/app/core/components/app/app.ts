4e720eb90702dcbb3658cc444ac90fa2fede84c9
Alejandro Cerezo <alce65@hotmail.es>
2026-09-21 13:03:22 +0200

    Relocate components with new scaffolding

--- ng22/projects/demo-01/src/app/core/components/app/app.ts
+++ ng22/projects/demo-01/src/app/core/components/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Sample } from './components/sample/sample';
+import { Sample } from '../sample/sample';
 
 @Component({
   imports: [RouterOutlet, Sample],
