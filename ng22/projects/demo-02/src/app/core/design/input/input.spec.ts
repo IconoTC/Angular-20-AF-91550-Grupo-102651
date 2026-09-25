d15b785f4e2e7289dc260ecc4f39d1bc5f482680
Alejandro Cerezo <alce65@hotmail.es>
2026-09-25 09:53:41 +0200

    Update test to input component

--- ng22/projects/demo-02/src/app/core/design/input/input.spec.ts
+++ ng22/projects/demo-02/src/app/core/design/input/input.spec.ts
@@ -1,16 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { form, FormField, required, SchemaPathTree } from '@angular/forms/signals';
+import { signal, Component } from '@angular/core';
 import { Input } from './input';
 
+interface MockModel {
+  name: string;
+}
+
+const  MOCK_MODEL = signal<MockModel>({
+    name: '',
+  });
+
+
+@Component({
+  selector: 'ind-test-host',
+  imports: [Input, FormField],
+  template: `
+    <form>
+      <ind-input
+        [label]="'Testing input'"
+        [type]="'text'"
+        [formField]="fieldTree.name"
+      />
+    </form>
+  `,
+})
+class TestHostComponent {
+  mockModel = MOCK_MODEL;
+  schema = (path: SchemaPathTree<MockModel>) => {
+    required(path.name, { message: 'Name is required' });
+  };
+
+  fieldTree = form(this.mockModel, this.schema);
+}
+
+
 describe('Input', () => {
-  let component: Input;
-  let fixture: ComponentFixture<Input>;
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Input],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Input);
+    fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
